9d82e7315f1ce9c4e2652cec73bd04f7967b4c84
rajesh n <rajesh.kit@gmail.com>
2025-01-27 13:37:15 +0530

    docker stage is added

--- src/main/java/com/exec/DevOpeController.java
+++ src/main/java/com/exec/DevOpeController.java
@@ -9,4 +9,12 @@ public class DevOpeController {
 	public String say() {
 		return "Hello how are u";
 	}
+	@GetMapping("/card")
+	public String payment() {
+		return "eneter card details. yes accepted";
+	}
+	@GetMapping("/upi")
+	public String payment1() {
+		return "eneter upi details. yes accepted";
+	}
 }
